c44bf637c04077204493dce7d5634c88d2ad69c8
frhanjav <farhan.javed006@gmail.com>
2024-12-07 23:27:58 +0530

    dfs

--- graph/dfs.cpp
+++ graph/dfs.cpp
@@ -1,40 +1,28 @@
-#include <iostream>
-#include <vector>
-using namespace std;
+class Solution
+{
+private:
+	void dfs(int node, vector<int> adj[], vector<int> vis[], vector<int> &ls)
+	{
+		vis[node] = 1;
+		ls.push_back(node);
 
-void DFSRec(vector<vector<int> > &adj, vector<bool> &visited, int s){
-    visited[s] = true;
-    cout << s << " ";
+		// Traverse
+		for (auto it : adj[node])
+		{
+			if (!vis[it])
+			{
+				dfs(it, adj, vis, ls);
+			}
+		}
+	}
 
-    for (vector<int>::iterator it = adj[s].begin(); it != adj[s].end(); ++it) {
-        int i = *it;
-        if (visited[i] == false)
-            DFSRec(adj, visited, i);
-    }
-}
-
-void DFS(vector<vector<int> > &adj, int s){
-    vector<bool> visited(adj.size(), false);
-    DFSRec(adj, visited, s);
-}
-
-void addEdge(vector<vector<int> > &adj, int s, int t){
-    adj[s].push_back(t); 
-    adj[t].push_back(s); 
-}
-
-int main(){
-    int V = 5; 
-    vector<vector<int> > adj(V);
-  
-    vector<pair<int, int>> edges = {{1, 2}, {1, 0}, {2, 0}, {2, 3}, {2, 4}};
-    for (int i = 0; i < edges.size(); ++i) {
-        addEdge(adj, edges[i].first, edges[i].second);
-    }
-
-    int s = 1;
-    cout << "DFS from source: " << s << endl;
-    DFS(adj, s);
-
-    return 0;
+public:
+	vector<int> dfsOfGraph(int V, vector<int> adj[])
+	{
+		int vis[n] = {0};
+		int start = 0;
+		vector<int> ls;
+		dfs(start, adj, vis, ls);
+		return ls;
+	}
 }
